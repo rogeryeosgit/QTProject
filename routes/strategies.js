ba22a50d2663626a11019a14bdb9261fab289c88
rogeryeosgit <roger@geekshacking.com>
2018-08-13 13:46:15 +0800

    fixing bugs on google strat

--- routes/strategies.js
+++ routes/strategies.js
@@ -8,7 +8,7 @@ var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 passport.use(new GoogleStrategy({
     clientID: '211325641937-lv80h7unjk3v2efi7i16rpgch9985ji5.apps.googleusercontent.com',
     clientSecret: 'EzLN_YyAiBX8uGg2fz27tiKb',
-    callbackURL: "http://qt.navigators.tech/profile"
+    callbackURL: "http://qt.navigators.tech/auth/google/callback"
   },
   function(accessToken, refreshToken, profile, done) {
        User.findOrCreate({ googleId: profile.id }, function (err, user) {
