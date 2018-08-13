53ecbba7ab9a2c0c71ccee20a9727cc638082ad2
rogeryeosgit <roger@geekshacking.com>
2018-08-13 13:35:11 +0800

    integrating google login strat

---
+++ routes/strategies.js
@@ -0,0 +1,20 @@
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(new GoogleStrategy({
+    clientID: '211325641937-lv80h7unjk3v2efi7i16rpgch9985ji5.apps.googleusercontent.com',
+    clientSecret: 'EzLN_YyAiBX8uGg2fz27tiKb',
+    callbackURL: "http://qt.navigators.tech/profile"
+  },
+  function(accessToken, refreshToken, profile, done) {
+       User.findOrCreate({ googleId: profile.id }, function (err, user) {
+         return done(err, user);
+       });
+  }
+  ));
+
+  module.exports = passport;
