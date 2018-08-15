6c93f4bf5693cb29c40f012f891c0d7bc043e516
rogeryeosgit <roger@geekshacking.com>
2018-08-15 15:52:30 +0800

    sessions management

--- routes/strategies.js
+++ routes/strategies.js
@@ -24,4 +24,14 @@ passport.use(new GoogleStrategy({
     }
 ));
 
+passport.serializeUser(function (user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function (id, done) {
+    User.findById(id, function (err, user) {
+        done(err, user);
+    });
+});
+
 module.exports = passport;
