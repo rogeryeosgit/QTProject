7d6c81178a8dcfc1d7cfe72b761fe85f1bc7a916
RogerY <roger@geekshacking.com>
2018-08-22 12:37:26 +0800

    adding headers

--- services/bible-retrieval.js
+++ services/bible-retrieval.js
@@ -10,13 +10,13 @@ var axios = require('axios');
 // var BRUrl = 'https://crossorigin.me/http://labs.bible.org/api/?passage=John+3:16-20&type=json';
 // var BRUrl = 'https://crossorigin.me/http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=John+1';
 const BRUrl = 'https://api.esv.org/v3/passage/html/';
+const header =  'Authorization: Token 4c2862b12a9a30a9abe905d718f1362a05ebb418';
 const params = {
-    Authorization: 'Token 4c2862b12a9a30a9abe905d718f1362a05ebb418',
     q: 'John+1'
 }
 
 console.log("-- Starting BR Service -- ");
-axios.get(BRUrl, params).then(data => console.log(data)).catch(error => console.log(error));
+axios.get(BRUrl, { headers: { header } }, params).then(data => console.log(data)).catch(error => console.log(error));
 console.log("-- Ending BR Service -- ");
 
 
