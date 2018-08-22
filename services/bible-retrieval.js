be10b3dffa9dd8385ff83b5021b2cdadc19e9d39
RogerY <roger@geekshacking.com>
2018-08-22 13:13:53 +0800

    try again

--- services/bible-retrieval.js
+++ services/bible-retrieval.js
@@ -10,13 +10,13 @@ var axios = require('axios');
 // var BRUrl = 'https://crossorigin.me/http://labs.bible.org/api/?passage=John+3:16-20&type=json';
 // var BRUrl = 'https://crossorigin.me/http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=John+1';
 const BRUrl = 'https://api.esv.org/v3/passage/html/';
-const header = 'Authorization: Token 4c2862b12a9a30a9abe905d718f1362a05ebb418';
+const header = {'Authorization': 'Token 4c2862b12a9a30a9abe905d718f1362a05ebb418'};
 const params = {
     q: 'John+1'
 }
 
 console.log("-- Starting BR Service -- ");
-axios.get(BRUrl, { headers: header }, params).then(function (data) { console.log(data) }).catch(function (error) { console.log(error) });
+axios.get(BRUrl, { headers: header }, { data: params }).then(function (data) { console.log(data) }).catch(function (error) { console.log(error) });
 console.log("-- Ending BR Service -- ");
 
 
