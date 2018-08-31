0d99bdcaa7befd7ab63fe7a6d70c6b6ac7167c35
RogerY <roger@geekshacking.com>
2018-08-31 23:30:53 +0800

    Securing

--- services/bible-retrieval.js
+++ services/bible-retrieval.js
@@ -11,7 +11,7 @@ var axios = require('axios');
 // var BRUrl = 'https://crossorigin.me/http://www.esvapi.org/v2/rest/passageQuery?key=IP&passage=John+1';
 const BRUrl = 'https://api.esv.org/v3/passage/html/';
 const header = {
-     'Authorization': 'Token 4c2862b12a9a30a9abe905d718f1362a05ebb418'
+     'Authorization': 'Token ' + process.env.Bible_Key
      };
 const params = {
     q: 'John+1'
