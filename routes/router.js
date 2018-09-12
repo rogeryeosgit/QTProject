a56138759f698d5e87f0eed43bf86a55f5ee6fec
RogerY <roger@geekshacking.com>
2018-09-12 22:52:58 +0800

    with try catch

--- routes/router.js
+++ routes/router.js
@@ -73,7 +73,12 @@ router.get('/profile', function (req, res, next) {
           err.status = 400;
           return next(err);
         } else {
-          return res.send(BRService.getPassage());
+          try {
+            let p = BRService.getPassage();
+          } catch (e) {
+            console.log(e);
+          }
+          return res.send(p);
           // return res.send('<h1>Name: </h1>' + user.username + '<h2>Mail: </h2>' + user.email + '<br><a type="button" href="/logout">Logout</a>')
           // return res.redirect('home.html');
         }
